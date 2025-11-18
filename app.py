45604b19d293c957edd32e4008730f328f732395
aryanzandi123 <46197403+aryanzandi123@users.noreply.github.com>
2025-11-18 03:36:56 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2011,4 +2011,4 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True, threaded=True, use_reloader=True)
+    app.run(host='127.0.0.1', port=5001, debug=True, threaded=True, use_reloader=True)
